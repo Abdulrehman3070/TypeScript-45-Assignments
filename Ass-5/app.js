3451c4fd5406844f21d3bbd6e8f59642ea94d62a
Abdul Rehman Khan <rehmankhana435@gmail.com>
2024-09-02 11:03:46 +0500

    Assignment-5
    Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.

---
+++ Ass-5/app.js
@@ -0,0 +1,4 @@
+//Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+var famous_person = "Quaid-e-Azam Muhammad Ali Jinnah";
+var message = "".concat(famous_person, " once Said, \u201CThink a hundred times before you take a decision, but once that decision is taken, stand by it as one man.\u201D");
+console.log(message);
