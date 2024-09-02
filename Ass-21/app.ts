04c1a381196d6bf258f8cac9f14b02966e60354d
Abdul Rehman Khan <rehmankhana435@gmail.com>
2024-09-02 14:59:59 +0500

    Assignment-21
    // They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.

---
+++ Ass-21/app.ts
@@ -0,0 +1,13 @@
+// They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
+
+// Create a car object 
+  let myCar = {
+    brand: "Toyota",
+    model: "Fortuner GRS",
+    year: 2023,
+    color: "White",
+  };
+  
+  console.log(myCar);
+  
