d40159e365d361cc3d7a3148544b0f2960cb63b1
Abdul Rehman Khan <rehmankhana435@gmail.com>
2024-09-02 11:31:45 +0500

    Assignment-8
    // Your output should simply be four lines with the number 8 appearing once on each line.

---
+++ Ass-8/app.js
@@ -0,0 +1,12 @@
+// Your output should simply be four lines with the number 8 appearing once on each line.
+var number = 8;
+var Line = "===================================";
+console.log(Line);
+console.log(number);
+console.log(Line);
+console.log(number);
+console.log(Line);
+console.log(number);
+console.log(Line);
+console.log(number);
+console.log(Line);
---
+++ Ass-8/app.ts
@@ -0,0 +1,16 @@
+// Your output should simply be four lines with the number 8 appearing once on each line.
+let  number : number=8;
+let Line = "==================================="
+
+console.log(Line);
+console.log(number);
+console.log(Line);
+console.log(number);
+console.log(Line);
+console.log(number);
+console.log(Line);
+console.log(number);
+console.log(Line);
+
+
+
