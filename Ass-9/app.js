d48d7e616c466ea9efb0d5c8dce4ca3fb2c13506
Abdul Rehman Khan <rehmankhana435@gmail.com>
2024-09-02 11:32:15 +0500

    Assignment-9
    //Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.

---
+++ Ass-9/app.js
@@ -0,0 +1,4 @@
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+var favoriteNumber = 18;
+var message = "My Favourite Number is ".concat(favoriteNumber);
+console.log(message);
