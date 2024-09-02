c77d17b095bfe4412ee4a66cadc5b03702ea8cbc
Abdul Rehman Khan <rehmankhana435@gmail.com>
2024-09-02 11:05:49 +0500

    Assignment-7
     Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.

---
+++ Ass-7/app.ts
@@ -0,0 +1,20 @@
+// Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+
+//Addition
+
+let Addition:number= 5+3;
+console.log(Addition);
+
+//subtraction
+let Subtraction:number= 11-3;
+console.log(Subtraction);
+
+//multiplication
+let multiplication:number=2*4;
+console.log(multiplication);
+
+//division
+let division:number= 16/2;
+console.log(division);
+
+
