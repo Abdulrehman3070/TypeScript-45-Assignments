2bdbecb2dc3c724cf182553d478e605c4b7c9675
Abdul Rehman Khan <rehmankhana435@gmail.com>
2024-09-02 15:00:59 +0500

    Assignment-22
    //Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.

---
+++ Ass-22/app.ts
@@ -0,0 +1,11 @@
+//Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
+
+
+
+let cars = ['Rocco', 'Rapter', 'Grande', 'Fortuner GRS'];
+
+// Intentional error: Accessing an index that does not exist
+console.log(cars[4]); // Outputs 'undefined'
+
+// Correcting the error: Accessing a valid index
+console.log(cars[2]); // Outputs 'Grande'
