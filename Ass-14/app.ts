a877470ca8724fadf46d2af34d5027653fdf4d91
Abdul Rehman Khan <rehmankhana435@gmail.com>
2024-09-02 14:30:41 +0500

    Assignment-14
    //Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.

---
+++ Ass-14/app.ts
@@ -0,0 +1,10 @@
+//Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+
+// Store guest names in an array
+let guests: string[] = ["Amir", "Saqib", "Danish"];
+
+// Print an invitation message for each guest
+console.log(`Dear ${guests[0]}, I would like to invite you to dinner. It would be an honor to have you as my guest.`);
+console.log(`Dear ${guests[1]}, I would like to invite you to dinner. It would be an honor to have you as my guest.`);
+console.log(`Dear ${guests[2]}, I would like to invite you to dinner. It would be an honor to have you as my guest.`);
+
