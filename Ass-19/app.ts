674ccfac36a97bd47804a84ed7b7d70188f0b7cf
Abdul Rehman Khan <rehmankhana435@gmail.com>
2024-09-02 14:33:39 +0500

    Assignment-19
    Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
    // I worked with program 16

---
+++ Ass-19/app.ts
@@ -0,0 +1,29 @@
+//Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+
+// I worked with program 16
+// Original guest list
+let guests: string[] = ["Amir", "Shahid", "Danish"];
+
+// Inform about the bigger dinner table
+console.log(`Dear ${guests[0]},Good news! We found a bigger dinner table, so we can invite more guests.`);
+console.log(`Dear ${guests[1]},Good news! We found a bigger dinner table, so we can invite more guests.`);
+console.log(`Dear ${guests[2]}.Good news! We found a bigger dinner table, so we can invite more guests.`);
+
+
+// Add more guests
+guests.unshift("Asif");
+guests.splice(Math.floor(guests.length / 2), 0, "Hassan"); // Add a new guest to the middle
+guests.push("Zain"); // Append a new guest to the end
+
+// Print a new set of invitation messages
+
+console.log(`Dear ${guests[0]}, I would like to invite you to dinner. It would be an honor to have you as my guest.`);
+console.log(`Dear ${guests[1]}, I would like to invite you to dinner. It would be an honor to have you as my guest.`);
+console.log(`Dear ${guests[2]}, I would like to invite you to dinner. It would be an honor to have you as my guest.`);
+console.log(`Dear ${guests[3]}, I would like to invite you to dinner. It would be an honor to have you as my guest.`);
+console.log(`Dear ${guests[4]}, I would like to invite you to dinner. It would be an honor to have you as my guest.`);
+console.log(`Dear ${guests[5]}, I would like to invite you to dinner. It would be an honor to have you as my guest.`);
+console.log(`Dear ${guests[6]}, I would like to invite you to dinner. It would be an honor to have you as my guest.`);
+
+// print a message indicating the number of people you are inviting to dinner.
+console.log(guests.length, "peoples are inviting on Dinner");
