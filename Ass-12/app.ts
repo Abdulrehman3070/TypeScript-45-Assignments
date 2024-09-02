16889650274391ca0ba144fcf58b451c938c11b1
Abdul Rehman Khan <rehmankhana435@gmail.com>
2024-09-02 14:29:36 +0500

    Assignment-12
    // Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.

---
+++ Ass-12/app.ts
@@ -0,0 +1,9 @@
+// Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+
+let names:string[]=["Ahmed","Ali","Danish","Saqib","Shahid"]
+
+console.log("Hello",names[0],"You have a good hand writing");
+console.log("Hello",names[1],"You have a good hand writing");
+console.log("Hello",names[2],"You have a good hand writing");
+console.log("Hello",names[3],"You have a good hand writing");
+console.log("Hello",names[4],"You have a good hand writing");
