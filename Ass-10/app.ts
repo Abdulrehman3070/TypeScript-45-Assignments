f7b0d8f2118d3c7d0593e895248b666f08a19a83
Abdul Rehman Khan <rehmankhana435@gmail.com>
2024-09-02 11:32:44 +0500

    Assignment-10
    //Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program d//Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:

---
+++ Ass-10/app.ts
@@ -0,0 +1,11 @@
+//Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program d//Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+
+//Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+console.log("Quaid-e-Azam Muhammad Ali Jinnah once Said, “Think a hundred times before you take a decision, but once that decision is taken, stand by it as one man.”"
+);
+
+//Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+let personName:string="Umar";
+console.log("Hello", personName, "would you like to learn some Typescript today?"  );
