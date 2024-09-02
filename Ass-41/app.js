6fe09743c72895490fead45e8eee76c6c26af764
Abdul Rehman Khan <rehmankhana435@gmail.com>
2024-09-03 01:42:58 +0500

    Assignment-41
    Magicians: Make a array of magician’s names. Pass the array to a function called show_magicians(), which prints the name of each magician in the array.

---
+++ Ass-41/app.js
@@ -0,0 +1,6 @@
+// Magicians: Make a array of magician’s names. Pass the array to a function called show_magicians(), which prints the name of each magician in the array.
+var magicians = ["David Copperfield", "Houdini", "Penn & Teller", "Dynamo"];
+function show_magicians(magicianss) {
+    console.log(magicianss);
+}
+show_magicians(magicians);
---
+++ Ass-41/app.ts
@@ -0,0 +1,13 @@
+// Magicians: Make a array of magician’s names. Pass the array to a function called show_magicians(), which prints the name of each magician in the array.
+
+
+function show_magicians(magicianss:string[]){
+   console.log(magicianss);
+   
+};
+
+const magicians: string[] = ["David Copperfield", "Houdini", "Penn & Teller", "Dynamo"];
+
+show_magicians(magicians)
+
+
