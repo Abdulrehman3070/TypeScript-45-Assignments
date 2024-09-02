870aaf0d089afd8bd310270318759e471f4ec1f9
Abdul Rehman Khan <rehmankhana435@gmail.com>
2024-09-03 01:45:24 +0500

    Assignment-45
    Cars: Write a function that stores information about a car in a Object. The function should always receive a manufacturer and a model name. It should then accept an arbitrary number of keyword arguments. Call the function with the required information and two other name-value pairs, such as a color or an optional feature. Print the Object that’s returned to make sure all the information was stored correctly.

---
+++ Ass-45/app.js
@@ -0,0 +1,23 @@
+// Cars: Write a function that stores information about a car in a Object. The function should always receive a manufacturer and a model name. It should then accept an arbitrary number of keyword arguments. Call the function with the required information and two other name-value pairs, such as a color or an optional feature. Print the Object that’s returned to make sure all the information was stored correctly.
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+function createCar(manufacturer, model, details) {
+    // Create the base car object
+    var car = __assign({ manufacturer: manufacturer, model: model }, details);
+    return car;
+}
+// Call the function with required and additional details
+var car1 = createCar("Toyota", "Camry", { Color: "Blue", Feature: "Sunroof" });
+var car2 = createCar("Ford", "Mustang", { Year: "2024", Engine: "V8" });
+// Print the objects to verify the information
+console.log(car1);
+console.log(car2);
